4e12f5beb5adbf4fcffca6856ceb0fcf9e1e673c
yalef <vlad1vaselkov@gmail.com>
2021-12-29 18:52:03 +0500

    Added delete and update views for Places

--- main/Places/urls.py
+++ main/Places/urls.py
@@ -3,7 +3,10 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path('home/', TemplateView.as_view(template_name="index.html"), name="home"),
     path("accounts/", include("allauth.urls")),
     path("create/", views.CreatePlaceView.as_view(), name="create"),
+    path("place/<slug:pk>/", views.UpdatePlaceView.as_view(), name="update"),
+    path("place/<slug:pk>/delete/", views.DeletePlaceView.as_view(), name="delete"),
 ]
--- main/Places/views.py
+++ main/Places/views.py
@@ -1,14 +1,27 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Place
 
 
 class CreatePlaceView(CreateView):
-    template_name='create.html'
+    template_name = 'create.html'
     model = Place
-    succes_url = reverse_lazy('/')
+    success_url = reverse_lazy('home/')
     fields=['title', 'description', 'location']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(CreatePlaceView, self).form_valid(form)
+
+
+class UpdatePlaceView(UpdateView):
+   model = Place
+   fields = ['title', 'description', 'location']
+   success_url = reverse_lazy('home/')
+   template_name = 'update.html'
+
+
+class DeletePlaceView(DeleteView):
+    template_name = 'delete.html'
+    model = Place
+    success_url = reverse_lazy('home')
